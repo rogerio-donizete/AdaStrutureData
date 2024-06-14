28cfd21439eb0c9ac75200546cdc33b1b8f18468
Bowie <marketing@letscode.com.br>
2024-06-14 14:47:32 -0300

    Solving problem one.

--- src/main/java/problemsSolving/ProblemOne.java
+++ src/main/java/problemsSolving/ProblemOne.java
@@ -1,5 +1,8 @@
 package problemsSolving;
 
+import java.util.Set;
+import java.util.Stack;
+
 /*
 dada uma string de expressão exp, escreva um programa para examinar se os
 pares e as ordens de {, }, (, ), [, ] estão corretos na expressão dada:
@@ -8,5 +11,29 @@ pares e as ordens de {, }, (, ), [, ] estão corretos na expressão dada:
    -> 2*(3+4+5*[2+3]/(2+(3+(7+5)*9)*6)*[5=8]+17)+2]
  */
 public class ProblemOne {
+
+    public static boolean solve(String expressiom){
+        Set<Character> openSymbols = Set.of('(', '{', '[');
+        Set<Character> closeSymbols = Set.of(')', '}', ']');
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: expressiom.toCharArray() ){
+            if(openSymbols.contains(c))stack.push(c);
+            if(closeSymbols.contains(c)){
+                if(stack.isEmpty())return false;
+                var open = stack.pop();
+                if (open == '(' && c != ')')return false;
+                if (open == '[' && c != ']')return false;
+                if (open == '{' && c != '}')return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve("2*(3+4+5*[2+3)]"));
+        System.out.println(solve("2*(3+4+5*[2+3])"));
+        System.out.println(solve("[2*(3+4+5*[2+3]/(2+(3+(7+5)*9)*6)*[5=8]+17)+2]"));
+    }
 
 }
